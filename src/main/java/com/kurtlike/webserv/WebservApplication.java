0762301a45aee8997654b61ff5bb43d7d631f303
Kurtlike <gordonfriman2000@gmail.com>
2021-03-19 14:41:43 +0300

    init

---
+++ src/main/java/com/kurtlike/webserv/WebservApplication.java
@@ -0,0 +1,13 @@
+package com.kurtlike.webserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kurtlike/webserv/WebservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kurtlike.webserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
