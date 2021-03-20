c19e320fceba5fd0804cee6ba94f00b7f6fe50c7
Kurtlike <gordonfriman2000@gmail.com>
2021-03-20 14:34:05 +0300

    add scp page with background

--- src/main/java/com/kurtlike/webserv/controllers/GreetingController.java
+++ src/main/java/com/kurtlike/webserv/controllers/GreetingController.java
@@ -1,17 +1,9 @@
 package com.kurtlike.webserv.controllers;
 
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class GreetingController {
-    @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "greeting";
 
-    }
 }
