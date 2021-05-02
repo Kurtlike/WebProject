ba3795b007f01e9ddf0e341300eedcbcd5a73cc5
Kurtlike <gordonfriman2000@gmail.com>
2021-05-02 16:08:08 +0300

    add interface to back

---
+++ src/main/java/com/kurtlike/webserv/MathLab3/GoodOldIO.java
@@ -0,0 +1,16 @@
+package com.kurtlike.webserv.MathLab3;
+
+import java.util.ArrayList;
+
+public interface GoodOldIO {
+    public ArrayList<String> getFunctions();
+    public ArrayList<String> getMethods();
+    public void setChosenFunction(String function);
+    public void setChosenMethod(String method);
+    public void setBorders(double left, double right);
+    public void setAccuracy(double accuracy);
+    public void setStartingNumberOfIterations(int n);
+    public double getAnswer();
+    public double getNumberOfDivisions();
+
+}
