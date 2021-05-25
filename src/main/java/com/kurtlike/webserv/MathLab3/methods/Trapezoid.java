577a022429bceb0866bc8adce3313bc8a91864c8
Cunning Jew <alexmax100500@gmail.com>
2021-05-25 14:18:09 +0300

    added console log for trapezoid fixed number check

--- src/main/java/com/kurtlike/webserv/MathLab3/methods/Trapezoid.java
+++ src/main/java/com/kurtlike/webserv/MathLab3/methods/Trapezoid.java
@@ -9,17 +9,24 @@ public class Trapezoid extends AbstractMethod implements Method{
         double psolution=0;
         double i1;
         double di = (right - left) / counter;
+        int step=1;
+        System.out.println(step+"\t");
         for (double i = left; i < right; i += di) {
            psolution+=di*(function.fun(i)+function.fun(i+di))/2;
         }
         counter++;
+        System.out.println(solution+"\t"+solution);
 
         while (!isEnd){
+            System.out.println(step+"\t");
             solution = 0;
             di = (right - left) / counter;
             for (double i = left; i < right; i += di) {
                 solution += di*(function.fun(i)+function.fun(i+di))/2;
             }
+            System.out.println(solution+"\t"+(solution-psolution)+"\t");
+            step++;
+
             if (Math.abs(solution - psolution) > accuracy) {
                 counter++;
                 psolution=solution;
