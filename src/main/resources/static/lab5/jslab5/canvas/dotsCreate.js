7cb2e8c8d27c006837e45eb6e6e5ad287ade0c15
Kurtlike <gordonfriman2000@gmail.com>
2021-06-06 11:32:36 +0300

    finish lab5.web 1.0

---
+++ src/main/resources/static/lab5/jslab5/canvas/dotsCreate.js
@@ -0,0 +1,5 @@
+function drawDotsForApproximate(){
+    for(let i = 0; i < dots.length; i++){
+        drawDot(dots[i].x, dots[i].y, 15);
+    }
+}
---
+++ src/main/resources/static/lab5/jslab5/presetInstall.js
@@ -0,0 +1,35 @@
+
+let numberOfDots = 12;
+let dots = [];
+presetsSelect.onchange = function (){
+    setPreset(presetsSelect.value);
+    setDots(dots);
+}
+function setPreset(Name){
+    switch (Name){
+        case "Parabola":{
+            for(let i = 0; i <= numberOfDots; i++){
+                let x = i - numberOfDots/2
+                let y = Math.pow(x,2);
+                dots[i] = {x, y}
+            }
+            break;
+        }
+        case "Sin":{
+            for(let i = 0; i <= numberOfDots; i++){
+                let x = i - numberOfDots/2
+                let y = Math.sin(x);
+                dots[i] = {x, y}
+            }
+            break;
+        }
+        case "Linear":{
+            for(let i = 0; i <= numberOfDots; i++){
+                let x = i - numberOfDots/2
+                let y = x;
+                dots[i] = {x, y}
+            }
+            break;
+        }
+    }
+}
